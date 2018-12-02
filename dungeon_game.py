4531be33e923a272463cc0a05af94025e7223cf4
Joe Samyn <19joe.samyn96@gmail.com>
2018-12-02 15:13:55 -0600

    deleted comments - JS

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,16 +1,6 @@
 import random
 import os
 
-# draw grid
-# pick random location for exit door
-# pick random location for player
-# pick radom location for monster
-# draw player in the grid
-# take input for movement
-# move player, unless invalid movement
-# check for win/loss
-# clear screen redraw grid
-
 CELLS = [(0, 0), (1, 0), (2, 0), (3 , 0), (4, 0),
          (0, 1), (1, 1), (2, 1), (3 , 1), (4, 1),
          (0, 2), (1, 2), (2, 2), (3 , 2), (4, 2),
