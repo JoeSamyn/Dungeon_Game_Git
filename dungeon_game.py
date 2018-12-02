ee773f700ca52946794e68ad57bfa91c0396037b
Joe Samyn <19joe.samyn96@gmail.com>
2018-12-02 15:11:59 -0600

    Updated ASCII graphics

--- dungeon_game.py
+++ dungeon_game.py
@@ -21,11 +21,21 @@ CELLS = [(0, 0), (1, 0), (2, 0), (3 , 0), (4, 0),
 def print_map(player):
     print("_"*5)
     tile = "|{}"
-    n = 0
     for cell in CELLS:
-        x, y = cell
-        if x < 4:
-
+        player_x, player_y = cell
+        if player_x < 4:
+            line_ending = ""
+            if cell == player:
+                output = tile.format("X")
+            else:
+                output = tile.format("_")
+        else:
+            line_ending = "\n"
+            if cell == player:
+                output = tile.format("X|")
+            else:
+                output = tile.format("_|")
+        print(output, end = line_ending)
 
 
 def get_locations():
@@ -34,24 +44,25 @@ def get_locations():
 def move_player(player, move):
     # get the players location
     player_x, player_y = player
-    # if move == LEFT, x-1
-    if move == 'LEFT' and player_x != 0:
+        # if move == LEFT, x-1
+    if move == 'LEFT' and player_x > 0:
         player_x -= 1
         return (player_x, player_y)
-    # if move == RIGHT x + 1
-    elif move == 'RIGHT' and player_x != 4:
+        # if move == RIGHT x + 1
+    elif move == 'RIGHT' and player_x < 4:
         player_x += 1
         return (player_x, player_y)
-    # if move == DOWN y - 1
-    elif move == 'DOWN' and player_y != 0:
+        # if move == DOWN y - 1
+    elif move == 'UP' and player_y > 0:
         player_y -= 1
         return (player_x, player_y)
-    # if move == UP y + 1
-    elif move == 'UP' and player_y != 4:
+        # if move == UP y + 1
+    elif move == 'DOWN' and player_y < 4:
         player_y += 1
         return (player_x, player_y)
     else:
-        return print("Invalid player move")
+        print("Invalid player move")
+        return (player_x, player_y)
 
 
 def clear_Screen():
@@ -61,46 +72,47 @@ def get_moves(player):
     moves = ["LEFT", "UP", "RIGHT", "DOWN"]
     player_x, player_y = player
     if player_y == 4:
-        print("You cannot move UP")
         moves.remove("UP")
     elif player_y == 0:
-        print("You cannot move DOWN")
         moves.remove("DOWN")
     elif player_x == 0:
-        print("You cannot move LEFT")
         moves.remove("LEFT")
     elif player_x == 4:
-        print("You cannot move RIGHT")
         moves.remove("RIGHT")
     else:
         return moves
     return (", ").join(moves)
 
 player, monster, door = get_locations()
-while True:
-    print("Welcome to the Dungeon!")
-    print("You're currently in room {}".format(player)) #fill with player position
-    print("You can move {}".format(get_moves(player))) # fill with available moves
-    print("Enter 'QUIT' to quit")
 
-    move = input("> ")
-    move = move.upper()
-
-    if move == 'QUIT':
-        break
-    # good move? change player position
-    else:
-        print_map(player)
-        player = move_player(player, move)
-        #print(move_player(player, move))
-
-
-    # hit door? They win
-    if player == door:
-        print("You made it to the door!  You Win!")
-        break
-    elif player == monster:
-        print("You hit the monster! Sorry you lose!:(")
-        break;
+print("Welcome to the Dungeon!")
+start = input("Press enter to start or 'Q' to quit.")
+if start.upper() == 'Q':
+    print("Okay see you next time!")
+elif start == "":
     clear_Screen()
-    # hit monster? they lose
+    while True:
+        print("You're currently in room {}".format(player)) #fill with player position
+        print("You can move {}".format(get_moves(player))) # fill with available moves
+
+        print_map(player)
+        move = input("> ")
+        move = move.upper()
+
+        if move == 'QUIT' or move == 'Q':
+            break
+        # good move? change player position
+        else:
+            clear_Screen()
+            player = move_player(player, move)
+            #print(move_player(player, move))
+
+
+        # hit door? They win
+        if player == door:
+            print("You made it to the door!  You Win!")
+            break
+        elif player == monster:
+            print("You hit the monster! Sorry you lose!:(")
+            break;
+        # hit monster? they lose
